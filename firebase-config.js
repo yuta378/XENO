0a70008a135b577ea06f9788d1623e98b1a6afd9
yuta378 <167378965+yuta378@users.noreply.github.com>
2026-06-01 14:51:54 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.__XENO_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDoZExE3itSfpEOLqzlMIdrk_pGRUQ3PJo",
-  authDomain: "xeno-2faa2.firebaseapp.com",
-  projectId: "xeno-2faa2",
-  storageBucket: "xeno-2faa2.firebasestorage.app",
-  messagingSenderId: "774878208003",
-  appId: "1:774878208003:web:e6d076407072dc8dc9ae36"
-};
